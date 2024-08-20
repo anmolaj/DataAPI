76039af95938448f05ca17443c134e5470e2bd3f
Anmol Jain <anmol.aj@gmail.com>
2024-08-20 16:20:22 +0530

    Add quality checker for duplicate with test

---
+++ app/utils/quality_checker.py
@@ -0,0 +1,21 @@
+from pandas import DataFrame
+from typing import List
+
+class QualityChecker:
+    def __init__(self, data: DataFrame, cols: List[str]= None):
+        
+        self.data = data
+        self.cols = cols
+
+    def duplicate_checker(self)-> DataFrame:
+        """
+        Check for duplicate rows in the data
+        """
+        if self.cols:
+            duplicates = self.data.duplicated(subset=self.cols)
+        else:
+            duplicates = self.data.duplicated()
+
+        if duplicates.any():
+            return duplicates
+        return None

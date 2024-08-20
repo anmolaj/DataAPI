f551ecdc68a66e9443d77583305c67f4119d1186
Anmol Jain <anmol.aj@gmail.com>
2024-08-20 16:05:04 +0530

    Add tests for utils

---
+++ app/utils/tests/test_reader.py
@@ -0,0 +1,25 @@
+import pytest
+from pandas import DataFrame
+from unittest.mock import patch
+from app.utils.reader import Reader
+from app.model.file_model import FileType
+
+def test_read_csv():
+    # Mock the return value of read_csv
+    # mock_read_csv.return_value = DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+    
+    reader = Reader(format="csv")
+    result = reader.read('app/utils/tests/data/dummy_path.csv')
+    
+    # Assert read_csv was called with the correct file path
+    # mock_read_csv.assert_called_once_with('dummy_path.csv')
+    # Assert the result is a DataFrame
+    assert isinstance(result, DataFrame)
+    # Assert the DataFrame content
+    assert (result['col1'] == [1, 2]).all()
+    assert (result['col2'] == [3, 4]).all()
+
+def test_unsupported_file_type():
+    reader = Reader(format='unsupported_format')
+    with pytest.raises(ValueError, match="Unsupported file type"):
+        reader.read('dummy_path')
